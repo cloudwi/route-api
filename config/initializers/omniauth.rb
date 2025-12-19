0b0fdfb9781389c03fac29f5433ba96a3b2520e0
cloudwi <cloudwiiiii@gmail.com>
2025-12-19 19:59:29 +0900

    code rabbit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,8 +10,9 @@ OmniAuth.config.silence_get_warning = true               # GET 요청에 대한 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Rails credentials에서 환경별 Kakao OAuth 설정 읽기
   # credentials.yml.enc 파일의 development.kakao.client_id 또는 production.kakao.client_id
-  client_id = Rails.application.credentials.dig(Rails.env.to_sym, :kakao, :client_id)
-  client_secret = Rails.application.credentials.dig(Rails.env.to_sym, :kakao, :client_secret)
+  # 테스트 환경에서는 더미 값 사용
+  client_id = Rails.application.credentials.dig(Rails.env.to_sym, :kakao, :client_id) || "test_client_id"
+  client_secret = Rails.application.credentials.dig(Rails.env.to_sym, :kakao, :client_secret) || "test_client_secret"
 
   # Kakao OAuth 프로바이더 등록
   # 첫 번째 인자: 전략 이름 (:kakao)
